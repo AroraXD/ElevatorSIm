7dd3911900ac7e937d1b7727cb754531e0fbf769
Justin king <jck2011@hotmail.co.uk>
2016-11-15 22:07:08 +0000

    Added elevator with strobing lights and working doors

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorControl : MonoBehaviour {
+
+	bool right = false;
+	float speed = 0.005f;
+
+	bool opening = false;
+	bool closing = false;
+	bool state = false;
+
+	void Start () {
+		if (gameObject.name == "Right Door"){
+			right = true;
+			speed *= -1;
+		}
+	}
+
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		if (!state && Input.GetKeyDown (KeyCode.Return)) {
+			opening = true;
+			closing = false;
+			state = true;
+		} else if (state && Input.GetKeyDown (KeyCode.Return)){
+			opening = false;
+			closing = true;
+			state = false;
+		}
+		if (opening) {
+			open ();
+		} else if (closing) {
+			close ();
+		}
+	}
+
+	protected void open(){
+		if (right) {
+			if (gameObject.transform.position.x <= -1.18f) {
+				opening = false;
+				gameObject.transform.localPosition = new Vector3 (-1.18f, 0f, 0f);
+			} else {
+				transform.Translate (speed, 0f, 0f);
+			}
+		} else {
+			if (gameObject.transform.position.x >= 1.18f) {
+				opening = false;
+				gameObject.transform.localPosition = new Vector3 (1.18f, 0f, 0f);
+			} else {
+				transform.Translate (speed, 0f, 0f);
+			}
+		}
+	} 
+
+	protected void close(){
+		if (right) {
+			if (gameObject.transform.position.x >= -0.405f) {
+				closing = false;
+				gameObject.transform.localPosition = new Vector3 (-0.405f, 0f, 0f);
+			} else {
+				transform.Translate (-speed, 0f, 0f);
+			}
+		} else {
+			if (gameObject.transform.position.x <= 0.405f) {
+				closing = false;
+				gameObject.transform.localPosition = new Vector3 (0.405f, 0f, 0f);
+			} else {
+				transform.Translate (-speed, 0f, 0f);
+			}
+
+		}
+	} 
+
+	public bool getState (){ return state;}
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+	float pos = 2.5f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+		pos = pos - 0.05f;
+		gameObject.transform.position = new Vector3 (0f,pos ,-1.5f);
+		if (pos < -0.5f) {
+			pos = 2.5f;
+		}
+
+	}
+}
